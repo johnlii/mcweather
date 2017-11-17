881c9325a8545b2d835d8d873b5e689f8b80c513
isaac <tex1333@163.com>
2017-11-17 16:13:43 +0800

    加入创建数据库和表的各项配置

---
+++ app/src/main/java/com/example/mcweather/db/County.java
@@ -0,0 +1,45 @@
+package com.example.mcweather.db;
+
+/**
+ * Created by 西安 on 2017/11/17.
+ */
+
+public class County {
+
+    private int id;
+    private String coutyName;
+    private String weatherId;
+    private int cityId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCoutyName() {
+        return coutyName;
+    }
+
+    public void setCoutyName(String coutyName) {
+        this.coutyName = coutyName;
+    }
+
+    public String getWeatherId() {
+        return weatherId;
+    }
+
+    public void setWeatherId(String weatherId) {
+        this.weatherId = weatherId;
+    }
+
+    public int getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
+}
